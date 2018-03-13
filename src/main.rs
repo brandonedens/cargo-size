a31db438da0fd47dd8681fe27bce039db72faf50
Brandon Edens <brandonedens@gmail.com>
2018-03-12 21:08:50 -0700

    Check in size process command execution.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+use std::ffi::OsStr;
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<String> = std::env::args().collect();
+    let args: Vec<&OsStr> = v.iter().map(|x| OsStr::new(x)).collect();
+
+    let child = Command::new("size")
+        .args(args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Failure to run size command.");
+
+    let output = child
+        .wait_with_output()
+        .expect("failure to wait on child");
+
+    println!("size exited {:?} with stdout: {:?} stderr: {:?}",
+             output.status.success(), output.stdout, output.stderr);
 }
