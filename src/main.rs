1d9072fa2d53d24050d652ec431536916475fc71
Brandon Edens <brandonedens@gmail.com>
2018-03-13 10:18:31 -0700

    Clean up output printing directly the results from stderr then stdout.

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,22 @@ fn run_size_cmd(args: &[&OsStr]) {
         .wait_with_output()
         .expect("failure to wait on child");
 
-    println!("size exited {:?} with stdout: {:?} stderr: {:?}",
-             output.status.success(), output.stdout, output.stderr);
+    let out = std::str::from_utf8(&output.stdout)
+        .expect("Failure to decode size command stdout");
+    let err = std::str::from_utf8(&output.stderr)
+        .expect("Failure to decode size command stderr");
+
+    if err.len() > 0 {
+        print!("{}", err);
+    }
+    if out.len() > 0 {
+        print!("{}", out);
+    }
 }
 
 fn main() {
     let v: Vec<String> = std::env::args().collect();
     let args: Vec<&OsStr> = v.iter().map(|x| OsStr::new(x)).collect();
 
-    run_size_cmd(&args);
+    run_size_cmd(&args[1..]);
 }
